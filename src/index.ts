f6cd7e486366c2de6275fcbb494b64f85ebf8218
gandalfthegray <stranavadavid@protonmail.com>
2026-01-12 13:54:26 +0100

    0.1.5 batch processing fixes

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './worker';
 export * from './metrics';
 export * from './keys';
 export * from './interfaces';
+export * from './batch-worker';
+export * from './stream-message-entity'
