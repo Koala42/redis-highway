19eb0efd16513533605dc1a59384b520bd94ea86
gandalfthegray <stranavadavid@protonmail.com>
2026-01-17 13:28:03 +0100

    Added vercel analytics to docs

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import { Home } from './pages/Home'
 import { GetStarted } from './pages/GetStarted'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics/>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
